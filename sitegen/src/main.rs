48b1b30bd695cf42b37aef9ccbc589379ba4b0de
Theodore Bauer <ted@commure.com>
2021-12-17 19:48:51 -0500

    Simple example working

--- sitegen/src/main.rs
+++ sitegen/src/main.rs
@@ -1,22 +1,25 @@
 use std::fs::File;
 use std::io::Write;
 fn main() -> std::io::Result<()> {
-    let index = b"<!DOCTYPE html>
-<head>
-<title>
-Notes
-</title>
-</head>
-
-<body>
-stuff
-</body>
+    let index = r#"
+<!DOCTYPE html>
+<!-- KaTeX requires the use of the HTML5 doctype. Without it, KaTeX may not render properly -->
 <html>
+  <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css" integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs" crossorigin="anonymous">
 
-</html>";
+    <!-- The loading of KaTeX is deferred to speed up page rendering -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.js" integrity="sha384-z1fJDqw8ZApjGO3/unPWUPsIymfsJmyrDVWC8Tv/a1HeOtGmkwNd/7xUS0Xcnvsx" crossorigin="anonymous"></script>
 
-    println!("Hello, world!");
+    <!-- To automatically render math in text elements, include the auto-render extension: -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/contrib/auto-render.min.js" integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR" crossorigin="anonymous"
+        onload="renderMathInElement(document.body);"></script>
+  </head>
+$$ 5 + 5 $$
+  ...
+</html>"#;
+
     let mut file = File::create("../index.html")?;
-    file.write_all(index)?;
+    file.write_all(index.as_bytes())?;
     Ok(())
 }
